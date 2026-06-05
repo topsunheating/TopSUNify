1d109b4ef36b3fb28b4556c40febb70d0bc60bfb
topsunheating <topsunheating@gmail.com>
2026-06-05 02:28:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -53,14 +53,14 @@ def main(page: ft.Page):
 
         radiator_size = ft.Dropdown(label="ابعاد رادیاتور", width=350, options=[ft.dropdown.Option(x) for x in RADIATOR_PRODUCTS.keys()])
         radiator_color = ft.Dropdown(label="طرح رادیاتور", width=350, options=[ft.dropdown.Option(x) for x in RADIATOR_COLORS])
-        radiator_orientation = ft.Dropdown(label="نوع نصب", width=350, options=[ft.dropdown.Option("افقی"), ft.dropdown.Option("عمودی"), ft.dropdown.Option("-")], value="-")
-        radiator_qty = ft.TextField(label="تعداد", width=350, value="1", keyboard_type=ft.KeyboardType.NUMBER)
+        radiator_orientation = ft.Dropdown(label="نوع نصب", width=250, options=[ft.dropdown.Option("افقی"), ft.dropdown.Option("عمودی"), ft.dropdown.Option("-")], value="-")
+        radiator_qty = ft.TextField(label="تعداد", width=150, value="1", keyboard_type=ft.KeyboardType.NUMBER)
 
         table = ft.DataTable(
             columns=[
-                ft.DataColumn(ft.Text("شرح")),
-                ft.DataColumn(ft.Text("تعداد")),
-                ft.DataColumn(ft.Text("جمع (تومان)"))
+                ft.DataColumn(ft.Text("شرح"), width=150)
+                ft.DataColumn(ft.Text("تعداد"), width=50)
+                ft.DataColumn(ft.Text("جمع (تومان)"), width=150)
             ],
             rows=[]
         )
